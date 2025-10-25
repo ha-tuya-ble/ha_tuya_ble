75f06f597c5826a2909c6235c92feaa85c1116fc
Johnny <jmdwestlake@yahoo.co.uk>
2025-10-25 20:25:52 +0100

    Support tr0kabuq

--- custom_components/tuya_ble/select.py
+++ custom_components/tuya_ble/select.py
@@ -368,6 +368,18 @@ mapping: dict[str, TuyaBLECategorySelectMapping] = {
                     ),
                 ),
             ],
+            "tr0kabuq": [  # Bluetooth Temperature Humidity Sensor
+                TuyaBLESelectMapping(
+                    dp_id=9,
+                    description=TemperatureUnitDescription(
+                        options=[
+                            UnitOfTemperature.CELSIUS,
+                            UnitOfTemperature.FAHRENHEIT,
+                        ],
+                        entity_registry_enabled_default=False,
+                    ),
+                ),
+            ],
         },
     ),
     "znhsb": TuyaBLECategorySelectMapping(
