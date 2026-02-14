6c1d8f925ceb8e58f9dea35d598a3f5a319bd6c8
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-02-14 03:24:17 +0000

    Black

--- custom_components/tuya_ble/lock.py
+++ custom_components/tuya_ble/lock.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.components.lock import LockEntity, LockEntityFeature, LockEntityDescription
+from homeassistant.components.lock import (
+    LockEntity,
+    LockEntityFeature,
+    LockEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
